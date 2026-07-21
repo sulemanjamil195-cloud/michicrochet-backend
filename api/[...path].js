e84c2520d34dfaa38b5f276dd5f1ed2e89450f16
Suleman <sulemanjamil195@gmail.com>
2026-07-21 18:42:58 +0500

    Add vercel configuration

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,15 @@
-// Vercel serverless entry point.
-const app = require('../app');
-const { sequelize } = require('../models');
+// // Vercel serverless entry point.
+// const app = require('../app');
+// const { sequelize } = require('../models');
 
-sequelize.authenticate()
-  .then(() => console.log('✅ DB connected'))
-  .catch(err => console.error('❌ DB connection failed:', err));
+// sequelize.authenticate()
+//   .then(() => console.log('✅ DB connected'))
+//   .catch(err => console.error('❌ DB connection failed:', err));
+
+// module.exports = app;
+
+const app = require("../app");
+
+console.log("✅ API FUNCTION LOADED");
 
 module.exports = app;
